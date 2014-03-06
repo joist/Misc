429403546b0d2e8d1d5377620dfe4d92b3260891
Joist <emailjoister@gmail.com>
2014-03-06 19:33:03 +1100

    Initial commit of clean Libgdx project

---
+++ Misc-desktop/src/temp/Main.java
@@ -0,0 +1,15 @@
+package temp;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
+public class Main {
+	public static void main(String[] args) {
+		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
+		cfg.title = "Misc";
+		cfg.width = 480;
+		cfg.height = 320;
+		
+		new LwjglApplication(new MyGdxGame(), cfg);
+	}
+}
